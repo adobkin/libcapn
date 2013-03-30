1d2b55fe397ddb917d8fc44be9c4eda3292c1f71
Anton Dobkin <anton.dobkin@gmail.com>
2013-03-30 22:29:16 +0700

    Updated win build script

--- win/build.bat
+++ win/build.bat
@@ -3,13 +3,18 @@
 if not exist "C:\Program Files\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" goto vc9
 echo call "C:\Program Files\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
 call "C:\Program Files\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
-goto :eof
+goto build
 
 :vc9
 if not exist "C:\Program Files\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" goto missing
 echo call "C:\Program Files\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
 call "C:\Program Files\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
-goto :eof
+
+:build
+cmake . -G "NMake Makefiles"
+nmake
+nmake install
+nmake zip
 
 :missing
 echo "Microsoft Visual Studio 9 and Microsoft Visual Studio 10 not found"
