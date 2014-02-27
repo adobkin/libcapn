c62c10884963a1f34377dc0dcc728fc8180b65df
Anton Dobkin <anton.dobkin@gmail.com>
2014-02-27 18:25:50 +0700

    Copyright year

--- src/apn_strings.h
+++ src/apn_strings.h
@@ -1,5 +1,5 @@
 /* 
- * Copyright (c) 2013, Anton Dobkin
+ * Copyright (c) 2013, 2104 Anton Dobkin
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
