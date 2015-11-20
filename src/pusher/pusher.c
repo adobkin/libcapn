d6993efef3f6eaa2151523da68f6d5804f6f97b2
Anton Dobkin <anton.dobkin@gmail.com>
2015-11-20 14:59:02 +0600

    Improvements and bugfixes
    - Added new option -'T'
    - Renamed option '-l' to '-i'

--- src/pusher/pusher.c
+++ src/pusher/pusher.c
@@ -23,14 +23,22 @@
 #include <errno.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
 #include <assert.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <ctype.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <termios.h>
+
 #include "apn.h"
+#include "apn_array.h"
 #include "apn_payload.h"
 #include "apn_strings.h"
+#include "apn_strerror.h"
 
-void __apn_logging(apn_log_levels level, const char *const message, uint32_t len) {
+static void __apn_logging(apn_log_levels level, const char *const message, uint32_t len) {
+    (void )len;
     const char *prefix = NULL;
     switch (level) {
         case APN_LOG_LEVEL_INFO:
@@ -46,15 +54,11 @@ void __apn_logging(apn_log_levels level, const char *const message, uint32_t len
     fprintf(stdout, "======> [apn][%s]: %s\n", prefix, message);
 }
 
-void __apn_invalid_token(const char *const token, uint32_t index) {
-    fprintf(stderr, "Invalid token: %s\n", token);
-}
-
-void __apn_token_free(void *data) {
+static void __apn_token_free(void *data) {
     free(data);
 }
 
-apn_array_t *__apn_split_tokens(char *const tokens) {
+static apn_array_t *__apn_split_tokens(char *const tokens) {
     apn_array_t *array = apn_array_init(20, __apn_token_free, NULL);
     if (array) {
         char *p = strtok(tokens, ":");
@@ -68,67 +72,126 @@ apn_array_t *__apn_split_tokens(char *const tokens) {
     return NULL;
 }
 
-static void __apn_pusher_usage(void) {
+apn_array_t *__apn_read_tokens_from_file(const char *const path) {
+    int fd = open(path, O_RDONLY);
+    if(fd < 0) {
+        return NULL;
+    }
+    struct stat fs;
+    if (-1 == fstat(fd, &fs)) {
+        close(fd);
+        return NULL;
+    }
+    char *buffer = mmap(0, fs.st_size, PROT_READ, MAP_SHARED, fd, 0);
+    if ((void*) -1 == buffer) {
+        close(fd);
+        return NULL;
+    }
 
-    fprintf(stderr, "apn-pusher - simple tool to send push notifications to iOS and OS X devices \n");
+    char *buffer_end = buffer + fs.st_size;
+    char *p_start = buffer;
+    char *p_end = buffer;
+
+    apn_array_t *tokens = apn_array_init(20, __apn_token_free, NULL);
+    if(!tokens) {
+        close(fd);
+        return NULL;
+    }
+
+    for(;;) {
+        if(*p_end != '\n') {
+            if (++p_end < buffer_end) {
+                continue;
+            }
+        }
+        size_t token_size = (p_end - p_start) + 1;
+        char *token = malloc(token_size);
+        if(!token) {
+            apn_array_free(tokens);
+            return NULL;
+        }
+        memset(token, 0, token_size);
+        apn_substr(token, p_start, token_size, 0, token_size - 1);
+        apn_array_insert(tokens, token);
+        if ((p_start = ++p_end) >= buffer_end) {
+            break;
+        }
+    }
+    munmap(buffer, fs.st_size);
+    close(fd);
+    return tokens;
+}
+
+static ssize_t __apn_getpass(char **password, size_t *n) {
+    struct termios old_termios;
+    struct termios new_termios;
+    if (0 != tcgetattr (STDIN_FILENO, &old_termios)) {
+        return -1;
+    }
+    new_termios = old_termios;
+    new_termios.c_lflag &= ~ECHO;
+    if (0 != tcsetattr(STDIN_FILENO, TCSAFLUSH, &new_termios)) {
+        return -1;
+    }
+    int read = getline (password, n, stdin);
+    tcsetattr (STDIN_FILENO, TCSAFLUSH, &old_termios);
+    return read;
+}
+
+static void __apn_pusher_usage(void) {
+    fprintf(stderr, "apn-pusher - simple tool to send push notifications to iOS and OS X devices\n");
     fprintf(stderr, "\n");
     fprintf(stderr, "Usage: apn-pusher [OPTION]\n");
     fprintf(stderr, "    -h Print this message and exit\n");
     fprintf(stderr, "    -c Path to .p12 file (required)\n");
-    fprintf(stderr, "    -p Passphrase for .p12 file (required)\n");
+    fprintf(stderr, "    -p Passphrase for .p12 file. Will be asked from the tty\n");
     fprintf(stderr, "    -d Use sandbox mode\n");
     fprintf(stderr, "    -m Body of the alert to send in notification\n");
     fprintf(stderr, "    -a Indicates content available\n");
     fprintf(stderr, "    -b Badge number to set with notification\n");
     fprintf(stderr, "    -s Name of a sound file in the app bundle\n");
-    fprintf(stderr, "    -l Name of an image file in the app bundle\n");
+    fprintf(stderr, "    -i Name of an image file in the app bundle\n");
     fprintf(stderr, "    -y Category name of notification\n");
-    fprintf(stderr, "    -t Device token(s). Separate multiple tokens with ':' (required)\n");
+    fprintf(stderr, "    -t Tokens, separated with ':' (required)\n");
+    fprintf(stderr, "    -T Path to file with tokens\n");
     fprintf(stderr, "    -v Make the operation more talkative\n");
 }
 
 int main(int argc, char **argv) {
-    uint8_t args_error = 0;
-    uint8_t ret = 0;
-
-    char *pass = NULL;
-    char *p12 = NULL;
-
-    apn_ctx_t *apn_ctx = NULL;
-    apn_payload_t *payload = NULL;
-
     if (argc < 2) {
         __apn_pusher_usage();
         return 1;
     }
 
+    assert(apn_library_init() == APN_SUCCESS);
     setvbuf(stderr, NULL, _IOLBF, 0);
 
-    assert(apn_library_init() == APN_SUCCESS);
-
-    if (NULL == (apn_ctx = apn_init())) {
-        printf("Unable to init context: %d\n", errno);
+    apn_ctx_t *apn_ctx = apn_init();
+    if (NULL == apn_ctx) {
+        fprintf(stderr, "Unable to init context: %d\n", errno);
         apn_library_free();
         return -1;
     }
 
-    if (NULL == (payload = apn_payload_init())) {
-        printf("Unable to init payload: %d\n", errno);
+    apn_payload_t *payload = apn_payload_init();
+    if (NULL == payload) {
+        fprintf(stderr, "Unable to init payload: %d\n", errno);
         apn_free(apn_ctx);
         apn_library_free();
         return -1;
     }
 
-    apn_array_t *tokens = NULL;
-
-    apn_set_log_level(apn_ctx, APN_LOG_LEVEL_ERROR);
-    apn_set_log_callback(apn_ctx, __apn_logging);
-    apn_set_invalid_token_callback(apn_ctx, __apn_invalid_token);
     apn_payload_set_priority(payload, APN_NOTIFICATION_PRIORITY_HIGH);
+    apn_set_behavior(apn_ctx, APN_OPTION_RECONNECT);
 
-    const char *const opts = "ahc:p:dm:b:s:l:e:y:t:v";
+    apn_array_t *tokens = NULL;
+    char *p12_pass = NULL;
+    char *p12 = NULL;
+    uint8_t ret = 0;
+    uint8_t rpassword = 0;
+
+    const char *const opts = "ahc:pdm:b:s:i:e:y:t:T:v";
     int c = -1;
-
     while ((c = getopt(argc, argv, opts)) != -1) {
         switch (c) {
             case 'h':
@@ -139,6 +202,7 @@ int main(int argc, char **argv) {
                 break;
             case 'b':
                 apn_payload_set_badge(payload, atoi(optarg));
+                break;
             case 'm':
                 apn_payload_set_body(payload, optarg);
                 break;
@@ -146,12 +210,12 @@ int main(int argc, char **argv) {
                 p12 = apn_strndup(optarg, strlen(optarg));
                 break;
             case 'p':
-                pass = apn_strndup(optarg, strlen(optarg));
+                rpassword = 1;
                 break;
             case 's':
                 apn_payload_set_sound(payload, optarg);
                 break;
-            case 'l':
+            case 'i':
                 apn_payload_set_launch_image(payload, optarg);
                 break;
             case 'y':
@@ -160,11 +224,21 @@ int main(int argc, char **argv) {
             case 't':
                 tokens = __apn_split_tokens(optarg);
                 break;
+            case 'T':
+                tokens = __apn_read_tokens_from_file(optarg);
+                if(!tokens) {
+                    char error[250] = {0};
+                    apn_strerror(errno, error, sizeof(error) - 1);
+                    fprintf(stderr, "Unable to parse file %s: %s (errno: %d).\n", optarg, error, errno);
+                    goto finish;
+                }
+                break;
             case 'a':
                 apn_payload_set_content_available(payload, 1);
                 break;
             case 'v':
-                apn_set_log_level(apn_ctx, APN_LOG_LEVEL_INFO | APN_LOG_LEVEL_ERROR | APN_LOG_LEVEL_DEBUG);
+                apn_set_log_callback(apn_ctx, __apn_logging);
+                apn_set_log_level(apn_ctx, APN_LOG_LEVEL_INFO | APN_LOG_LEVEL_ERROR);
                 break;
             case '?':
                 if (optopt == 'c') {
@@ -174,25 +248,38 @@ int main(int argc, char **argv) {
                 } else {
                     fprintf(stderr, "Unknown option character `\\x%x'.\n", optopt);
                 }
-                args_error = 1;
-                break;
+                goto finish;
         }
-    }
-
-    if (args_error) {
-        ret = 1;
-        goto finish;
     }
 
     if (p12) {
-        if (!pass) {
-            fprintf(stderr, "Missing passphrase option\n");
+        if(rpassword) {
+            fprintf(stderr, "Enter .p12 file password: ");
+            size_t p12_pass_len = 1024;
+            p12_pass = malloc(p12_pass_len);
+            if(!p12_pass) {
+                ret = 1;
+                goto finish;
+            }
+            memset(p12_pass, 0, p12_pass_len);
+            ssize_t read = __apn_getpass(&p12_pass, &p12_pass_len);
+            if (-1 == read) {
+                ret = 1;
+                goto finish;
+            }
+            if(p12_pass[read - 1] == '\n') {
+                p12_pass[read -1] = '\0';
+            }
+        }
+        fprintf(stderr, "\n");
+        if (!p12_pass || *p12_pass == '\n') {
+            fprintf(stderr, "Missing passphrase\n");
             ret = 1;
             goto finish;
         }
-        apn_set_pkcs12_file(apn_ctx, p12, pass);
+        apn_set_pkcs12_file(apn_ctx, p12, p12_pass);
     } else {
-        fprintf(stderr, "Missing .p12 file option\n");
+        fprintf(stderr, "Missing .p12 file\n");
     }
 
     if (!tokens || apn_array_count(tokens) == 0) {
@@ -206,24 +293,35 @@ int main(int argc, char **argv) {
                 apn_error_string(errno), errno);
         ret = 1;
     } else {
-        if (APN_ERROR == apn_send2(apn_ctx, payload, tokens)) {
+        apn_array_t *invalid_tokens = NULL;
+        if (APN_ERROR == apn_send(apn_ctx, payload, tokens, &invalid_tokens)) {
             ret = 1;
             fprintf(stderr, "Could not send push: %s (errno: %d)\n", apn_error_string(errno), errno);
         } else {
-            fprintf(stderr, "Notification was sucessfully sent!\n");
+            fprintf(stderr, "Notification was sucessfully sent to %u device(s)\n",
+                    apn_array_count(tokens) - ((invalid_tokens) ? apn_array_count(invalid_tokens) : 0));
+        }
+
+        if (invalid_tokens) {
+            fprintf(stderr, "\n");
+            fprintf(stderr, "Invalid tokens:\n");
+            uint32_t i = 0;
+            for (; i < apn_array_count(invalid_tokens); i++) {
+                fprintf(stderr, "    %u. %s\n", i, apn_array_item_at_index(invalid_tokens, i));
+            }
+            fprintf(stderr, "\n");
+            apn_array_free(invalid_tokens);
         }
     }
 
     finish:
-    apn_strfree(&pass);
+    apn_strfree(&p12_pass);
     apn_strfree(&p12);
 
     apn_free(apn_ctx);
     apn_payload_free(payload);
-    apn_library_free();
     apn_array_free(tokens);
     apn_library_free();
 
     return ret;
-
 }
