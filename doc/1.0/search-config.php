6a598329198fa263753163c7969cc0408cc59c60
Anton Dobkin <antonio@diego.local>
2016-01-19 22:15:17 +0600

    Documentations

---
+++ doc/1.0/search-config.php
@@ -0,0 +1,22 @@
+<script language="php">
+
+$config = array(
+  'PROJECT_NAME' => "libcapn",
+  'GENERATE_TREEVIEW' => false,
+  'DISABLE_INDEX' => false,
+);
+
+$translator = array(
+  'search_results_title' => "Search Results",
+  'search_results' => array(
+    0 => "Sorry, no documents matching your query.",
+    1 => "Found <b>1</b> document matching your query.",
+    2 => "Found <b>\$num</b> documents matching your query. Showing best matches first.",
+  ),
+  'search_matches' => "Matches:",
+  'search' => "Search",
+  'split_bar' => "",
+  'logo' => "Generated on Fri Feb 28 2014 17:29:46 for libcapn by&#160;\n<a href=\"http://www.doxygen.org/index.html\">\n<img class=\"footer\" src=\"doxygen.png\" alt=\"doxygen\"/></a> 1.8.6 ",
+);
+
+</script>
